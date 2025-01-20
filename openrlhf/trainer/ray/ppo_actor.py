3f95993b75eddbc8f257a8782cdae08a4597cda2
Zack Ankner <zackankner@gmail.com>
2025-01-20 06:22:03 +0000

    don't shuffle dataloaders when training policy with Ray

--- openrlhf/trainer/ray/ppo_actor.py
+++ openrlhf/trainer/ray/ppo_actor.py
@@ -284,7 +284,7 @@ class ActorModelRayActor(BasePPORole):
             prompts_data, self.tokenizer, strategy, input_template=args.input_template
         )
         self.prompts_dataloader = strategy.setup_dataloader(
-            self.prompts_dataset, args.rollout_batch_size // strategy.world_size, True, True
+            self.prompts_dataset, args.rollout_batch_size // strategy.world_size, True, False # Set shuffle to False since preset data order
         )
 
         if args.pretrain_data:
